e97075cacf860e8eb6669e0028e88b54c2eddc62
Ivan <i85chiosa@gmail.com>
2023-01-27 15:39:24 +0100

    Last Change

--- Javascript-Basic-Code/10-Functions/starter/script.js
+++ Javascript-Basic-Code/10-Functions/starter/script.js
@@ -2,18 +2,168 @@
 
 
 
-const info = function (firstName, lastName) {
+///////////////////////////////////////
+// Default Parameters
+const bookings = [];
 
-    return `Mein  name ist ${firstName}}, und mein Last name ist ${lastName}`;
+const createBooking = function (
+    flightNum,
+    numPassengers = 1,
+    price = 199 * numPassengers
+) {
+    // ES5
+    // numPassengers = numPassengers || 1;
+    // price = price || 199;
+
+    const booking = {
+        flightNum,
+        numPassengers,
+        price,
+    };
+    console.log(booking);
+    bookings.push(booking);
 };
 
-console.log('Ivan', 'Chiosa');
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
 
-const info2 = (first, last) => {
-    return `Mein  name ist ${first}}, und mein Last name ist ${last}`;
+createBooking('LH123', undefined, 1000);
 
+
+///////////////////////////////////////
+// How Passing Arguments Works: Values vs. Reference
+const flight = 'LH234';
+const jonas = {
+    name: 'Jonas Schmedtmann',
+    passport: 24739479284,
 };
-console.log('Mario', 'Rossi');
 
-const greet = () => console.log('Hey Ivan');
-document.querySelector('.poll').addEventListener('click', greet)
+// const checkIn = function (flightNum, passenger) {
+//     flightNum = 'LH999';
+//     passenger.name = 'Mr. ' + passenger.name;
+
+//     if (passenger.passport === 24739479284) {
+//         alert('Checked in');
+//     } else {
+//         alert('Wrong passport!');
+//     }
+// };
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = jonas;
+
+// const newPassport = function (person) {
+//     person.passport = Math.trunc(Math.random() * 100000000000);
+// };
+
+// newPassport(jonas);
+// checkIn(flight, jonas);
+
+
+///////////////////////////////////////
+// Functions Accepting Callback Functions
+const oneWord = function (str) {
+    return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+};
+
+
+const user = function (str) {
+    return str.toLowerCase();
+}
+console.log(user('Hey Ivan ich wunche dir viel erfolgt'));
+
+
+const upperFirst = function (str) {
+    const [first, second, ...rest] = str.split(' ');
+    return [first.toUpperCase(), second.toUpperCase(), ...rest].join(' ');
+};
+console.log(upperFirst('hey IVan ich wunche dir wiel erfolg'));
+
+
+// Higher-order function
+const transformer = function (str, str2, fn) {
+    console.log(`Original string: ${str}`);
+    console.log(`Transformed string: ${fn(str)} `);
+
+    console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', 'Hallo', upperFirstWord);
+transformer('JavaScript is the best!', 'Ivan', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+    console.log('👋');
+};
+document.body.addEventListener('click', high5);
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+
+const greet = function (greeting) {
+    return function (name, vorname) {
+        console.log(`${greeting}... mein name ist ${name} und mein vorname ist ${vorname}`);
+    };
+};
+
+
+const greetingHey = greet('Hey');
+greetingHey('Ivan', 'Chiosa');
+
+greet('Hallo')('Ivan', 'Schulz');
+
+
+// Arrow functions 1
+const greet1 = greetings => {
+    return function (name, vorname) {
+        console.log(`${greetings}... mein name ist ${name} und mein vorname ist ${vorname}`);
+    };
+};
+
+greet1('Guten Tag')('Ecaterina', 'Chiosa')
+
+// Arrow function 2 
+const greet2 = greeting => (namen, vorname) => console.log(`${greeting}... mein name ist ${namen} und mein vorname ist ${vorname}`)
+
+greet2('Buna ziua')('Nina', 'Chiosa')
+
+
+const lufthansa = {
+    airline: 'Lufthansa',
+    iataCode: 'LH',
+    bookings: [],
+    book(flightNum, name) {
+        console.log(`${name} booked a seat on ${this.airline} ${this.iataCode} ${flightNum}`);
+        this.bookings.push({ flight: `${this.airline} ${this.iataCode} ${flightNum}`, name })
+    }
+};
+
+lufthansa.book(123, 'Ivan Chiosa');
+console.log(lufthansa);
+
+
+const addTaxRate = function (rate) {
+    return function (value) {
+        return value + value * rate;
+    };
+};
+
+const addVAT2 = addTaxRate(0.40);
+console.log(addVAT2(100));
+
+(function () {
+    console.log('Hey Ivan');
+})();
+
+(() => console.log('Hey Basti'))();
--- Javascript-Basic-Code/JavaSc/index.js
+++ Javascript-Basic-Code/JavaSc/index.js
@@ -2,34 +2,84 @@
 
 
 
-const arr = [1, 2, 3, 4];
-console.log(arr);
-const newArr = [...arr];
-console.log(newArr);
-const [a, b, ...others] = [1, 2, 3, 4, 5];
-console.log(a, b, others);
+// const arr = [1, 2, 3, 4];
+// console.log(arr);
+// const newArr = [...arr];
+// console.log(newArr);
+// const [a, b, ...others] = [1, 2, 3, 4, 5];
+// console.log(a, b, others);
 
 
-const passengers = 'iVaN';
-const passengersLower = passengers.toLowerCase();
-const passengersCorrect = passengersLower[0].toUpperCase() + passengersLower.slice(1);
-console.log(passengersCorrect);
-const passengersSliced = passengers.slice(8);
-console.log(passengersSliced);
+// const passengers = 'iVaN';
+// const passengersLower = passengers.toLowerCase();
+// const passengersCorrect = passengersLower[0].toUpperCase() + passengersLower.slice(1);
+// console.log(passengersCorrect);
+// const passengersSliced = passengers.slice(8);
+// console.log(passengersSliced);
 
 
-const announcements = 'All passengers come to boarding door 23. Boarding door 23';
-// console.log(announcements.replace('door', 'Gate'));
-console.log(announcements.replaceAll('door', 'Gate'));
+// const announcements = 'All passengers come to boarding door 23. Boarding door 23';
+// // console.log(announcements.replace('door', 'Gate'));
+// console.log(announcements.replaceAll('door', 'Gate'));
 
 
-const capitalizeName = function (name) {
-    const names = name.split(' ');
-    const namesUpper = [];
+// const capitalizeName = function (name) {
+//     const names = name.split(' ');
+//     const namesUpper = [];
 
-    for (const n of names) {
-        namesUpper.push(n[0].toUpperCase() + n.slice(1));
-    }
-    console.log(namesUpper.join(' '));
+//     for (const n of names) {
+//         namesUpper.push(n[0].toUpperCase() + n.slice(1));
+//     }
+//     console.log(namesUpper.join(' '));
+// }
+// capitalizeName('ivan as a web developer');
+
+
+const user = function (str) {
+    return str.replace(/ /g, ' ').toLowerCase();
+};
+
+console.log(user('Ivan is a web developer'));
+
+const upperFirst = function (str) {
+    const [first, second, ...rest] = str.split(' ');
+    return [first.toUpperCase(), second.toUpperCase(), ...rest].join(' ');
+};
+
+console.log(upperFirst('Ivan is a web developer'));
+
+const transformer = function (str, fn) {
+    console.log(`Original string: ${str}`);
+    console.log(`Original string: ${fn(str)}`);
 }
-capitalizeName('ivan as a web developer');
+
+transformer('Ivan is a web developer', user)
+transformer('Ivan is a web developer', upperFirst)
+
+
+// const user1 = function (str) {
+//     const splitWort = str.split(' ');
+//     return str.split(' ').toUpperCase([0]).join(' ')
+// }
+
+
+// console.log(user1('hello world'));
+
+function titleCase(string) {
+    return string[0].toUpperCase() + string.slice(1).toLowerCase();
+}
+console.log(titleCase('download prIce History'));
+
+
+const oneWords = function (str) {
+    const [first, ...last] = str.split(' ');
+    console.log([first.toUpperCase(), ...last].join(' '));
+};
+oneWords('hello World')
+
+
+const firstLetter = function () {
+    let str = 'hello World';
+    return str[0].toUpperCase() + str.slice(1).toLowerCase();
+};
+console.log(firstLetter('hello World'));
