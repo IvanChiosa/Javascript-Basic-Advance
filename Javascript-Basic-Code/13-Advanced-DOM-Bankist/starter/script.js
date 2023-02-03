a5f20d5898f8535c91f0d1b200ec25b118cf99ea
Ivan <i85chiosa@gmail.com>
2023-02-03 14:28:22 +0100

    Lezion 12

--- Javascript-Basic-Code/13-Advanced-DOM-Bankist/starter/script.js
+++ Javascript-Basic-Code/13-Advanced-DOM-Bankist/starter/script.js
@@ -32,3 +32,20 @@ document.addEventListener('keydown', function (e) {
 
 // Page navigation
 
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+
+// tabs.forEach(t => t.addEventListener('click', () => console.log('TAB')))
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+
+  if (!clicked) return;
+
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+})
+
